e5bd2c7c809e433d4894c2643540661a5681897b
Fatou Diop SOLY <solyfatoudiop@gmail.com>
2024-01-26 02:33:13 +0100

    firstCommit

--- SamaTalibe_AngularProject/src/app/app-routing.module.ts
+++ SamaTalibe_AngularProject/src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { AccueilComponent } from '../app/Composants/UserNonConnect/accueil/accueil.component';
 import { AproposComponent } from '../app/Composants/UserNonConnect/apropos/apropos.component';
 import { LoginComponent } from './Composants/Auth/login/login.component';
+import { RegisterComponent } from './Composants/Auth/register/register.component';
 
 const routes: Routes = [
   { path: 'accueil', component: AccueilComponent },
   { path: 'a-propos', component: AproposComponent },
   { path: 'login', component: LoginComponent},
   { path: '', redirectTo: '/accueil', pathMatch: 'full' },
+  { path: 'register' , component: RegisterComponent },
+  
 ];
 
 @NgModule({
